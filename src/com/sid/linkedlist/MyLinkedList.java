221b10056e35296395c73b2f84f006721482fb0a
smbiradar92 <smbiradar92@gmail.com>
2022-03-24 00:55:06 +0530

    Initial commit

---
+++ src/com/sid/linkedlist/MyLinkedList.java
@@ -0,0 +1,17 @@
+package com.sid.linkedlist;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyLinkedList {
+
+	public static void main(String[] args) {
+
+		List<Integer> intList = new LinkedList<>();
+		
+}
+
+}
+
+
